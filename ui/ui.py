a33d51b23a71be04949eac8b4d39edbb4c89e7ad
ElizabethSeth <setharth1998@gmail.com>
2025-12-02 14:18:05 +0000

    try

--- ui/ui.py
+++ ui/ui.py
@@ -7,10 +7,10 @@ import os, re , mimetypes, tempfile, urllib.parse
 import io
 from typing import List
 
-#API_URL = "http://api:8003"
+API_URL = "http://api:8003"
 
 
-API_URL = "https://reports-service-api-t5nmsc3a7a-uc.a.run.app"
+#API_URL = "https://reports-service-api-t5nmsc3a7a-uc.a.run.app"
 
 
 def return_apikey():
