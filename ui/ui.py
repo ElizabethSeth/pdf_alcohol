532ad6677ae3327181e5978943dd00aa64bbea51
ElizabethSeth <setharth1998@gmail.com>
2026-03-11 08:34:50 +0000

    ui port

--- ui/ui.py
+++ ui/ui.py
@@ -566,5 +566,5 @@ with gr.Blocks(
     )
 
 if __name__ == "__main__":
-    app.launch(server_name="0.0.0.0", server_port=8001)
+    app.launch(server_name="0.0.0.0", server_port=8080)
 # docker logs -f gradio-1 auth=check_login, auth_message=LOGIN_HTML
