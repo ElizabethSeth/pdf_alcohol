3546648b7d65bdf7993c5e77f810a9de1b82acda
ElizabethSeth <setharth1998@gmail.com>
2026-03-12 15:31:06 +0000

    api

--- ui/ui.py
+++ ui/ui.py
@@ -11,7 +11,7 @@ from ast import Load
 import sys
 import os
 sys.path.append(os.path.abspath(".."))
-API_URL = os.getenv("API_URL")
+#API_URL = os.getenv("API_URL")
 #API_URL = "http://main:8003"
 
 API_URL = "https://generate-reports.api.elsth.com"
